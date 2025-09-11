d9eb8d17fae142a9e4235284bf59f50ec5fb6e73
alex-dv9 <204044518+alex-dv9@users.noreply.github.com>
2025-09-11 22:20:22 +0300

    commit #2

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import shutil
 import tempfile
 import os
+import json
+
 from datetime import date
 from pathlib import Path
 
-# directory_to_archive, its_parent_directory, save_as(optional)
-dirs = [
-    ["test_directory", "dotfiles", ""]
-]
+from time import sleep
+
+with open("config.json", "r", encoding="utf-8") as config:
+    dirs = json.load(config)
 
 def main():
     # replace path with realpath
@@ -17,14 +19,19 @@ def main():
     # move every dir & files in it to tempdir
     with tempfile.TemporaryDirectory() as tmpdir:        
         for dir in dirs:
-            dest = os.path.join(tmpdir, os.path.basename(dir[0]))
+            dest = os.path.join(tmpdir, dir[1], os.path.basename(dir[0]))
             
+            if not os.path.exists(dest):
+                os.makedirs(dest, exist_ok=True)
+
             # if path is file
             if os.path.isfile(dir[0]):
                 shutil.copy(dir[0], dest)
             # if path is directory
             if os.path.isdir(dir[0]):
-                shutil.copytree(dir[0], dest)
+                shutil.copytree(dir[0], dest, dirs_exist_ok=True)
+
+            sleep(1000)
 
         shutil.make_archive(f"backup-{date.today().isoformat()}", "zip", tmpdir)
 
