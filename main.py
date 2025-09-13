ca5f7882f60c6ca8f4328c6e6b24fab52a46410c
segfaultchan <segfaultchan@github.com>
2025-09-13 16:41:35 +0300

    making archive

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from ast import For
-import shutil
+import shutil #, sys
 from pathlib import Path
+from sys import exception
 from colorama import Fore
 
+# archive
+arch_name="its_backup"
+arch_format="zip"
+arch_dir="./backup/"
+
+# backup dirs/fils
 dirs = [
     ["~/.config","config"],
     ["/etc/shadowsocks-rust/","shadowsocks"],
@@ -11,6 +17,11 @@ dirs = [
 files = [
     ["~/.zshrc","dotfiles/zshrc"]
 ]
+# exceptions
+exceptions = ["config/discord",]
+
+#def args_check():
+#    args = sys.argv[1:]
 
 def dir_backup():
     for x in dirs:
@@ -20,7 +31,7 @@ def dir_backup():
             continue
         x[0] = str(new.resolve())
         try:
-            shutil.copytree(src=x[0],dst=x[1],ignore_dangling_symlinks=True,dirs_exist_ok=True)
+            shutil.copytree(src=x[0],dst=arch_dir+x[1],ignore_dangling_symlinks=True,dirs_exist_ok=True)
         except shutil.Error as error:
             print(Fore.YELLOW,"socket files or something are not copied")
         except FileNotFoundError as error:
@@ -35,16 +46,33 @@ def file_backup():
             continue
         x[0] = str(new.resolve())
         try:
-            shutil.copyfile(src=x[0],dst=x[1])
+            shutil.copyfile(src=x[0],dst=arch_dir+x[1])
         except FileNotFoundError as error:
             raise(error)
         except IsADirectoryError:
             print(Fore.RED,"u must write a path/to/file to files",Fore.WHITE)
 
+def excepts_delete():
+    for x in exceptions:
+        try:
+            shutil.rmtree(arch_dir+x)
+        except FileNotFoundError as error:
+            print(Fore.RED,error,Fore.WHITE)
+            continue
+
+def archive():
+    shutil.make_archive(base_name=arch_name,format=arch_format,root_dir="./")
+    
+def final():
+    shutil.rmtree(arch_dir)
 
 def main():
+#    args_check()
     dir_backup()
     file_backup()
+    excepts_delete()
+    archive()
+    final()
 
 if __name__ == "__main__":
     main()
